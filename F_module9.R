e5a97c87f2c0fe0539d1d06192838ea7a9cd6ab9
tylerkoloski <tjk2005@wildcats.unh.edu>
2017-10-25 16:17:28 -0400

    Set up framework and answered Question 1

---
+++ F_module9.R
@@ -0,0 +1,21 @@
+# This file will fullfill the requirements of Assingment 9 wherin use 
+#of version control such as Git and Github with R studio 
+#integration was explored.
+# Authors: Jessie Briggs and Tyler Koloski
+# last modified: 10/25/17
+
+#Question 1
+library(ggplot2)
+data(msleep)
+?msleep
+
+head(msleep)
+summary(msleep)
+unique(msleep$vore)
+# There are 4 unique types of diet as well as NAs 
+
+#Question 2
+
+
+
+#Question 3
